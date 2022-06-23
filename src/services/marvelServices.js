0883f463482cfea9df4f334199f9073017a91e9d
SHOX <theshoxjaxon@gmail.com>
2022-06-24 03:32:32 +0500

    rewrite fetch api stucture

--- src/services/marvelServices.js
+++ src/services/marvelServices.js
@@ -1,4 +1,6 @@
 class MarvelServices {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/"
+    _apiKey = "apikey=ea3892a4a38fd40f6edd334273023729"
     getRecurce = async (url) => {
         let res = await fetch(url);
 
@@ -10,9 +12,11 @@ class MarvelServices {
     }
 
     getAllCharacters = () => {
-        return this.getRecurce("https://gateway.marvel.com:443/v1/public/characters?apikey=ea3892a4a38fd40f6edd334273023729");
+        return this.getRecurce(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
     }
-
+    getCharacter = (id) => {
+        return this.getRecurce(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
 }
 
 export default MarvelServices
