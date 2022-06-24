d95a1b390daab2e09460f3911421693223152c57
Shoxjaxon Xolmurodov <97885211+theshoxjaxon@users.noreply.github.com>
2022-06-24 22:05:47 +0500

    added random charset part

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import MarvelServices from './services/marvelServices';
-import './style/style.scss';
-
-const marvelService = new MarvelServices();
-marvelService.getAllCharacters().then(res => res.data.results.forEach(element => {
-  console.log(element);
-}))
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/App";
+// import MarvelServices from "./services/marvelServices";
+import "./style/style.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
