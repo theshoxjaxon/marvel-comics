90245485745b47f022a3fb9bed9b6b4a16989a73
Shoxjaxon Xolmurodov <97885211+theshoxjaxon@users.noreply.github.com>
2022-06-26 00:58:02 +0500

    fixed all  bugs in transformCharacter

--- src/services/marvelServices.js
+++ src/services/marvelServices.js
@@ -1,33 +1,36 @@
 class MarvelServices {
-    _apiBase = "https://gateway.marvel.com:443/v1/public/"
-    _apiKey = "apikey=ea3892a4a38fd40f6edd334273023729"
-    getRecurce = async (url) => {
-        let res = await fetch(url);
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=ea3892a4a38fd40f6edd334273023729";
+  getRecurce = async (url) => {
+    let res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-        }
-
-        return await res.json()
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
-    getAllCharacters = () => {
-        return this.getRecurce(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    }
-    getCharacter = async (id) => {
-        const res = await this.getRecurce(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res)
-    }
-    _transformCharacter = (res) => {
-        return {
-            name: res.data.results[0].name,
-            description: res.data.results[0].description,
-            thumbnail:
-                res.data.results[0].thumbnail.path + "." + res.data.results[0].thumbnail.extension,
-            homepage: res.data.results[0].urls[0].url,
-            wiki: res.data.results[0].urls[1].url,
-        }
-    }
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getRecurce(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+  getCharacter = async (id) => {
+    const res = await this.getRecurce(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
 }
 
-export default MarvelServices
+export default MarvelServices;
